1a59fcb58ca12075f593fc239885f0d3c71ce797
Brian J Griglak <griglb@yahoo.com>
2022-12-24 19:29:42 -0800

    Add Problem 52

---
+++ ProjectEuler0052/ProjectEuler0052.cpp
@@ -0,0 +1,71 @@
+// ProjectEuler0052.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <set>
+
+#include "big_int.h"
+
+
+std::set<int64_t> get_permuted_multiples() {
+    for (BigInt curr_val{1}; true ; curr_val += 1) {
+        auto tmp = curr_val.get_digits();
+        std::multiset<int8_t> digits{tmp.begin(), tmp.end()};
+
+        BigInt val2{curr_val};
+        val2 *= 2;
+        tmp = val2.get_digits();
+        std::multiset<int8_t> digits2{tmp.begin(), tmp.end()};
+
+        if (digits != digits2)
+            continue;
+
+        BigInt val3{curr_val};
+        val3 *= 3;
+        tmp = val3.get_digits();
+        std::multiset<int8_t> digits3{tmp.begin(), tmp.end()};
+
+        if (digits != digits3)
+            continue;
+
+        BigInt val4{curr_val};
+        val4 *= 4;
+        tmp = val4.get_digits();
+        std::multiset<int8_t> digits4{tmp.begin(), tmp.end()};
+
+        if (digits != digits4)
+            continue;
+
+        BigInt val5{curr_val};
+        val5 *= 5;
+        tmp = val5.get_digits();
+        std::multiset<int8_t> digits5{tmp.begin(), tmp.end()};
+
+        if (digits != digits5)
+            continue;
+
+        BigInt val6{curr_val};
+        val6 *= 6;
+        tmp = val6.get_digits();
+        std::multiset<int8_t> digits6{tmp.begin(), tmp.end()};
+
+
+        if (digits != digits6)
+            continue;
+
+        return { curr_val.to_int(), val2.to_int(), val3.to_int(), val4.to_int(), val5.to_int(), val6.to_int()};
+    }
+
+    return {};
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    auto multiples = get_permuted_multiples();
+    for (const auto &mult : multiples)
+        std::cout << mult << " ";
+    std::cout << std::endl;
+}

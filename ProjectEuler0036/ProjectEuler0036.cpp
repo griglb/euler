715912d6dad42ee058d06be1b1b1c541941eb133
Brian J Griglak <brian.griglak@bluerivertech.com>
2023-01-04 15:17:38 -0700

    Add problem descriptions

--- ProjectEuler0036/ProjectEuler0036.cpp
+++ ProjectEuler0036/ProjectEuler0036.cpp
@@ -1,5 +1,7 @@
-// ProjectEuler0036.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+// Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+// (Please note that the palindromic number, in either base, may not include leading zeros.)
+
 
 #include <algorithm>
 #include <iostream>
--- ProjectEuler0039/ProjectEuler0039.cpp
+++ ProjectEuler0039/ProjectEuler0039.cpp
@@ -1,5 +1,8 @@
-// ProjectEuler0039.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// If p is the perimeter of a right angle triangle with integral length sides,
+// {a,b,c}, there are exactly three solutions for p = 120.
+// {20,48,52}, {24,45,51}, {30,40,50}
+// For which value of p ≤ 1000, is the number of solutions maximised?
+
 
 #include <iostream>
 #include <set>

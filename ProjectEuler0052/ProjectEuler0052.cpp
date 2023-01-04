0aeab053818fbc3ef1679457055af7193611b78f
Brian J Griglak <brian.griglak@bluerivertech.com>
2023-01-04 16:05:45 -0700

    Add problem descriptions

--- ProjectEuler0052/ProjectEuler0052.cpp
+++ ProjectEuler0052/ProjectEuler0052.cpp
@@ -1,5 +1,7 @@
-// ProjectEuler0052.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// It can be seen that the number, 125874, and its double, 251748,
+// contain exactly the same digits, but in a different order.
+// Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+
 
 #include <iostream>
 #include <set>

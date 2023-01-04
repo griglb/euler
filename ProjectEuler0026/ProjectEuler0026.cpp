414535006d9e7e97b3e71da1f3eb4cf3639f6da6
Brian J Griglak <brian.griglak@bluerivertech.com>
2023-01-04 15:06:40 -0700

    Add problem descriptions.

--- ProjectEuler0026/ProjectEuler0026.cpp
+++ ProjectEuler0026/ProjectEuler0026.cpp
@@ -1,5 +1,18 @@
-// ProjectEuler0026.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// A unit fraction contains 1 in the numerator.
+// The decimal representation of the unit fractions with denominators 2 to 10 are given:
+//     1/2	= 	0.5
+//     1/3	= 	0.(3)
+//     1/4	= 	0.25
+//     1/5	= 	0.2
+//     1/6	= 	0.1(6)
+//     1/7	= 	0.(142857)
+//     1/8	= 	0.125
+//     1/9	= 	0.(1)
+//     1/10	= 	0.1 
+// Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle.
+// It can be seen that 1/7 has a 6-digit recurring cycle.
+// Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+
 
 #include <iostream>
 #include <utility>
@@ -84,7 +97,6 @@ std::pair<BigInt::Digits, BigInt::Digits> get_decimal(int numerator, int denomin
 }
 
 
-
 int main()
 {
     std::cout << "Hello World!\n";
@@ -112,5 +124,4 @@ int main()
         std::cout << static_cast<int>(digit);
     std::cout << std::endl;
     std::cout << longest_inverse << "\t" << longest_repeat.size() << std::endl;
-
 }

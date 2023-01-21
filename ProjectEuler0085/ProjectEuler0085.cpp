510099a7797a9512e729dfce145fe68e43cf8e3d
Brian J Griglak <griglb@yahoo.com>
2023-01-21 16:16:17 -0700

    Adding empty projects for Problems 85-90

---
+++ ProjectEuler0085/ProjectEuler0085.cpp
@@ -0,0 +1,27 @@
+// By counting carefully it can be seen that a rectangular grid measuring 3 by 2 contains eighteen rectangles :
+//
+//     +--+  +  +    +--+--+  +    +--+--+--+
+//     |  |          |     |       |        |
+//     +--+  +  +    +--+--+  +    +--+--+--+
+//
+//     +  +  +  +    +  +  +  +    +  +  +  +
+//         6              4             2
+//
+//     +--+  +  +    +--+--+  +    +--+--+--+
+//     |  |          |     |       |        |
+//     +  +  +  +    +  +  +  +    +  +  +  +
+//     |  |          |     |       |        |
+//     +--+  +  +    +--+--+  +    +--+--+--+
+//          3             2             1
+//  
+// Although there exists no rectangular grid that contains exactly two million rectangles,
+// find the area of the grid with the nearest solution.
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}

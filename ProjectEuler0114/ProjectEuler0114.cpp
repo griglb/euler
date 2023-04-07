780221b0c1f0e3d1f78e01531ff47d7bec18a72b
Brian Griglak <brian.griglak@bluerivertechnology.com>
2023-04-06 20:02:49 -0700

    Added thoughts to Problem 114

--- ProjectEuler0114/ProjectEuler0114.cpp
+++ ProjectEuler0114/ProjectEuler0114.cpp
@@ -18,7 +18,82 @@
 //        in general it is permitted to mix block sizes.For example, on a row
 //        measuring eight units in length you could use red(3), grey(1), and red(4).
 
+
 #include <iostream>
+
+// Length 3 - 2 options
+//      OOO         XXX
+// Length 4 - 4 options
+//      OOOO
+//      OXXX        XXXO
+//      XXXX
+// Length 5 - 7 options 
+//      OOOOO
+//      OOXXX       OXXXO       XXXOO
+//      OXXXX       XXXXO
+//      XXXXX
+// Length 6 - 11 options
+//      OOOOOO
+//      OOOXXX      OOXXXO      OXXXOO      XXXOOO
+//      OOXXXX      OXXXXO      XXXXOO
+//      OXXXXX      XXXXXO
+//      XXXXXX
+// Length 7 - 17 options
+//      OOOOOOO
+//      OOOOXXX     OOOXXXO     OOXXXOO     OXXXOOO     XXXOOOO
+//      OOOXXXX     OOXXXXO     OXXXXOO     XXXXOOO
+//      OOXXXXX     OXXXXXO     XXXXXOO
+//      OXXXXXX     XXXXXXO
+//      XXXXXXX
+//      XXXOXXX
+// Length 8 - 24 options
+//      OOOOOOOO
+//      OOOOOXXX    OOOOXXXO    OOOXXXOO    OOXXXOOO    OXXXOOOO    XXXOOOOO
+//      OOOOXXXX    OOOXXXXO    OOXXXXOO    OXXXXOOO    XXXXOOOO
+//      OOOXXXXX    OOXXXXXO    OXXXXXOO    XXXXXOOO
+//      OOXXXXXX    OXXXXXXO    XXXXXXOO
+//      OXXXXXXX    XXXXXXXO
+//      XXXXXXXX
+//      XXXOXXXX    XXXXOXXX
+// Length 9 - 42 options
+//      000000000
+//      28 options with 1 red block
+//      OOXXXOXXX
+//      OXXXOOXXX       OXXXOXXXO
+//      XXXOOOXXX       XXXOOXXXO       XXXOXXXOO
+//      OXXXOXXXX
+//      XXXOOXXXX       XXXOXXXXO
+//      OXXXXOXXX
+//      XXXXOOXXX       XXXXOXXXO
+//      XXXXOXXXX
+// Length 10 - 
+//      OOOOOOOOOO
+//      36 options with 1 red block
+//      OOOXXXOXXX
+//      OOXXXOOXXX      OOXXXOXXXO
+//      OXXXOOOXXX      OXXXOOXXXO      OXXXOXXXOO
+//      XXXOOOOXXX      XXXOOOXXXO      XXXOOXXXOO      XXXOXXXOOO
+//      OOXXXOXXXX
+//      OXXXOOXXXX      OXXXOXXXXO
+//      XXXOOOXXXX      XXXOOXXXXO      XXXOXXXXOOO
+//      OXXXOXXXXX
+//      XXXOOXXXXX      XXXOXXXXXO
+//      OOXXXXOXXX
+//      OXXXXOOXXX      OXXXXOXXXO
+//      XXXXOOOXXX      XXXXOOXXXO      XXXXOXXXOO
+//      OXXXXOXXXX
+//      XXXXOOXXXX      XXXXOXXXXO
+//      OXXXXXOXXX
+//      XXXXXOOXXX      XXXXXOXXXO
+//      OXXXXOXXXX
+//      XXXXOOXXXX      XXXXOXXXXO
+
+// For Length L>2:
+//  - 0 red blocks: 1 all Os
+//  - 1 red block:  (L-2)'th triangle number = (L-2)(L-1)/2
+// For L>6:
+//  - 2 red blocks:  
+
 
 int main()
 {

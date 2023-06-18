7cc7280d042d93300c66afe80a590b3b97e85ec2
Brian J Griglak <griglb@yahoo.com>
2023-06-18 17:46:46 -0600

    Solve Problem 135

--- ProjectEuler0129/ProjectEuler0129.cpp
+++ ProjectEuler0129/ProjectEuler0129.cpp
@@ -12,8 +12,14 @@
 
 
 #include <iostream>
+#include <numeric>
+#include <unordered_set>
+#include <vector>
 
+#include "big_int.h"
 #include "chunked_prime_helper.h"
+#include "fraction.h"
+#include "prime_helper.h"
 
 
 // Prime factorizations of the first few R(k) values:
@@ -34,28 +40,176 @@
 // To get a solution where A(n) > 1,000,000 would require factoring a number with
 // over 1,000,000 digits.  More sophisticated number theory techniques are needed.
 
+// If A(n) = x, then A(n^p) = n^(p-1) * A(n)
+// For composite n = a x b, A(n) = LCM(A(a), A(b)) if GCD(a, b) = 1
+
+// 1287 = 33 x 39 = (3 x 11) x (3 x 13) = 3^2 x 11 x 13
+//   A(3) = 3
+//   A(11) = 2
+//   A(13) = 6
+//   A(33) = 6
+//   A(39) = 6
+//   A(1287) = 18
+//   A(1287) = LCM( LCM(3 * A(3), A(11)), A(13) )
+//           = LCM( LCM(9, 2), 6) = LCM(18, 6) = 18
+
+// 4719 = 33 x 143 = (3 x 11) x (11 x 13)
+//   A(3) = 3
+//   A(11) = 2
+//   A(13) = 6
+//   A(33) = 6
+//   A(143) = 6
+//   A(4719) = 66
+//   A(4719) = LCM( LCM(A(3), 11 * A(11)), A(13) )
+//           = LCM( LCM(3, 22), 6) = lCM(66, 6) = 66
+
+class Finder {
+public:
+    Finder(uint64_t max_value) {
+        R_small = std::vector<uint64_t>{ 0,
+                                         1,
+                                         11,
+                                         111,
+                                         1'111,
+                                         11'111,
+                                         111'111,
+                                         1'111'111,
+                                         11'111'111,
+                                         111'111'111,
+                                         1'111'111'111,
+                                         11'111'111'111,
+                                         111'111'111'111,
+                                         1'111'111'111'111,
+                                         11'111'111'111'111,
+                                         111'111'111'111'111,
+                                         1'111'111'111'111'111,
+                                         11'111'111'111'111'111,
+                                         111'111'111'111'111'111,
+                                         1'111'111'111'111'111'111
+        };
+
+        R_big.reserve(max_value);
+        for (const auto& R : R_small) {
+            R_big.push_back(BigInt{ R });
+        }
+        helper_.get_primes(max_value);
+    }
+
+    uint64_t A(uint64_t n) {
+        // First check the R(k) values that can fit in 64-bits
+        for (uint64_t i = 2; i < 20; ++i) {
+            if (0 == R_small[i] % n)
+                return i;
+        }
+
+        for (uint64_t i = 20; i < 2 * n; ++i) {
+            if (i == R_big.size()) {
+                BigInt tmp;
+                tmp.set_digits(BigInt::Digits(i, 1));
+                R_big.push_back(tmp);
+            }
+
+            BigInt R = R_big[i];
+            BigInt quot = R / n;
+            quot *= n;
+            if (R == quot)
+                return i;
+        }
+
+        throw "Need more digits!";
+    }
+
+    uint64_t A_fast(uint64_t n) {
+        if (a_for_n.find(n) != a_for_n.end())
+            return a_for_n.at(n);
+
+        auto factors = helper_.get_factorization_fast(n);
+
+        std::vector<uint64_t> terms;
+        terms.reserve(factors.size());
+
+        for (const auto& [prime, exp] : factors) {
+            if (a_for_n.find(prime) == a_for_n.end())
+                a_for_n[prime] = A(prime);
+            terms.push_back(a_for_n.at(prime));
+            for (uint64_t e = 1; e < exp; ++e)
+                terms.back() *= prime;
+        }
+
+        uint64_t A{ 1 };
+        for (const auto& t : terms)
+            A = lcm(A, t);
+        a_for_n[n] = A;
+        return A;
+    }
+
+private:
+    PrimeHelper helper_;
+    std::vector<uint64_t> R_small;
+    std::vector<BigInt> R_big;
+    std::map < uint64_t, uint64_t> a_for_n;
+
+};
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 
+    //{
+    //    Finder finder(1000);
+    //    std::vector<uint64_t> numbers{ 3,   7,   11,   13,   17,  19,  21,  23,  29,   33,   39, 51, 57, 77,
+    //                                   9,  49,  121,  169,  289, 361, 441, 529, 841, 1089, 1521,  // squares
+    //                                  27, 343, 1331, 2197, 4913,  // cubes
+    //                                  143, 1287, 4719  };  // products with gcd > 1
+    //    for (const auto& n : numbers) {
+    //        std::cout << "A(" << n << ") = " << finder.A(n) << "\t" << finder.A_fast(n) << std::endl;
+    //    }
+    //}
+
     {
-        ChunkedPrimeHelper<10'000'000> helper;
-//        helper.get_primes(1e8);
-        uint64_t r{ 1 };
-        for (uint64_t k = 1; k < 12; ++k) {
-            auto factors = helper.get_factorization_fast(r);
-            std::cout << k << "\t" << r << "\t";
+        constexpr uint64_t min_value{ 999'500 };
+        constexpr uint64_t max_value{ 1'100'000 };
+
+        PrimeHelper helper;
+        auto primes = helper.get_primes(max_value);
+        for (uint64_t n = min_value; n <= max_value; ++n) {
+            if ((0 == n % 2) || (0 == n % 5))
+                continue;
+
+            auto factors = helper.get_factorization_fast(n);
+
+            if (factors.size() < 5)
+                continue;
+
+            std::cout << n << " = ";
             for (const auto& [prime, exp] : factors) {
-                if (1 == exp)
-                    std::cout << prime << " x ";
-                else
+                if (exp > 1)
                     std::cout << prime << "^" << exp << " x ";
+                else
+                    std::cout << prime << " x ";
             }
             std::cout << std::endl;
-
-            r *= 10;
-            r += 1;
         }
     }
+    return 0;
+    {
+        constexpr uint64_t max_value{ 10'000 };
+
+        Finder finder{ max_value };
+        PrimeHelper helper;
+        auto primes = helper.get_primes(max_value);
+
+        for (uint64_t n = 3; n < max_value; ++n) {
+            // Skip all multiples of 2 or 5, so gcd(n, 10) = 1
+            if ((0 == n % 2) || (0 == n % 5))
+                continue;
+
+            auto a = finder.A_fast(n);
+            std::cout << n << "\t" << a << std::endl;
+            if (a > 1'000'000)
+                break;
+        }
+    }
+
 }
---
+++ ProjectEuler0135/ProjectEuler0135.cpp
@@ -0,0 +1,93 @@
+// Given the positive integers, x, y, and z, are consecutive terms of an arithmetic progression,
+// the least value of the positive integer, n, for which the equation,
+//      x^2 - y^2 - z^2 = n
+// has exactly two solutions is n=27:
+//      34^2 - 27^2 - 20^2 = 12^2 - 9^2 - 6^2 = 27
+//
+// It turns out that n=1155 is the least value which has exactly ten solutions.
+//
+// How many values of n less than one million have exactly ten distinct solutions?
+
+
+#include <cmath>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+// The first question is how to bound the search space.
+// The terms x, y, z form a decreasing arithmetic progression, since n > 0.
+// This means x - y == y - z == k.
+// Let's call x = y + k & z = y - k, the the expression for n becomes:
+//      (y + k)^2 - y^2 - (y - k)^2 = n
+//      y^2 + 2ky + k^2 - y^2 - (y^2 - 2ky + k^2) = n
+//      4ky - y^2 = n
+// Check this with the examples:
+//      34^2 - 27^2 - 20^2 = 27  ->  y = 27, k = 7 -> 4*7*27 - 27^7 = 28*27 - 27*27 = 27
+//      12^2 - 9^2 - 6^2 = 27    ->  y = 9,  k = 3 -> 4*3*9 - 9^2 = 12*9 - 9*9 = 3*9 = 27
+
+// Can the quadratic equation be applied here?
+//      4*k*y - y^2 = 27
+//      y^2 - 4*k*y + 27 = 0
+//      y = (4k +- sqrt(16*k^2 - 4*1*27)) / 2
+//      y = 2k +- sqrt(4*k^2 - 27)
+//      k = 7 -> y = 14 +- sqrt(4*49 - 27) = 14 +- sqrt(196 - 27) = 14 +- sqrt(169) = 14 +- 13 = { 27, 1 }
+//      k = 3 -> y = 6 +- sqrt(36 - 27) = 6 +- sqrt(9) = 6 +- 3 = { 9, 3 }
+// In both cases, the sum yields the correct value of y, while the difference results in z <= 0.
+
+
+class Solver {
+public :
+    Solver(int64_t max_n) {
+        squares_.reserve(max_n + 1);
+        for (int64_t n = 1; n <= max_n; ++n)
+            squares_.push_back(n * n);
+    }
+
+    std::vector<uint64_t> get_counts(uint64_t max_n) {
+        std::vector<uint64_t> ret(max_n + 1, 0);
+
+        for (int64_t k = 1; k < max_n; ++k) {
+            if (k % 1000 == 0)
+                std::cout << k << std::endl;
+            for (int64_t y = k + 1; y < max_n; ++y) {
+                const int64_t n = 4 * k * y - y * y;
+                if ((n >= 0) && (n < max_n))
+                    ++ret[n];
+            }
+        }
+
+        return ret;
+    }
+
+private:
+    std::vector<int64_t> squares_;
+
+};
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    //{
+    //    Solver solver(1200);
+    //    auto counts = solver.get_counts(1200);
+    //    for (size_t n = 0; n < counts.size(); ++n)
+    //        std::cout << n << "\t" << counts[n] << std::endl;
+    //}
+
+    {
+        Solver solver(1'000'000);
+        auto counts = solver.get_counts(1'000'000);
+        size_t num{ 0 };
+        for (size_t n = 0; n < counts.size(); ++n) {
+            if (counts[n] == 10) {
+                std::cout << n << "\t" << counts[n] << std::endl;
+                ++num;
+            }
+        }
+        std::cout << "There are " << num << " values of n with exactly 10 sequences." << std::endl;
+    }
+
+}

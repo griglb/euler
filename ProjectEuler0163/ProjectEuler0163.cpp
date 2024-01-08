a5e39e5cf18238cc9648fc6fd4620f4a51534882
Brian J Griglak <griglb@yahoo.com>
2024-01-08 13:50:40 -0700

    Wokring on Problems 163, 163

--- ProjectEuler0163/ProjectEuler0163.cpp
+++ ProjectEuler0163/ProjectEuler0163.cpp
@@ -2,23 +2,24 @@
 // vertex to the middle of the opposite side, such as in the size 1 triangle in
 // the sketch below.
 // 
-//                                                      *
+//                                                      *A
 //                                                     /|\
 //                                                    / | \
 //                                                   /  |  \
-//                                                  +_  |  _+
-//              *                                  /   -|-   \
-//             /|\                                /   _ * _   \
-//            / | \                              /_ -   |   - _\
-//           /  |  \                            *-------+-------*
-//          +_  |  _+                          /|\- _   |   _ -/|\
-//         /   -|-   \                        / | \   - * -   / | \
-//        /   _ * _   \                      /  |  \ _ -|- _ /  |  \
-//       /_ -   |   - _\                    +_  |  _+   |   +_  |  _+
+//                                                 B+_  |  _+C
+//              *A                                 /   -|-   \
+//             /|\                                /   _ *D_   \
+//            / | \                              /_ -   |F  - _\
+//           /  |  \                           E*-------+-------*G
+//         B+_  |  _+C                         /|\- _   |   _ -/|\
+//         /   -|-   \                        / | \   - *H-   / | \
+//        /   _ *D_   \                      /  |  \ _ -|- _ /  |  \
+//       /_ -   |   - _\                   I+_  |  _+J  |  K+_  |  _+L
 //      *-------+-------*                  /   -|-   \  |  /   -|-   \
-//           Size 1                       /   _ * _   \ | /   _ * _   \
-//                                       /_ -   |   - _\|/_ -   |   - _\
+//      E       F       G                 /   _ *M_   \ | /   _ *N_   \
+//           Size 1                      /_ -   |   - _\|/_ -   |   - _\
 //                                      *-------+-------*-------+-------*
+//                                      O       P       Q       R       S
 //                                                   Size 2
 //
 // Sixteen triangles of either different shape or size or orientation or location
@@ -36,6 +37,71 @@
 //                      T(2) = 104
 // Find T(36).
 
+// The interior angles at every vertex are either 30, 60, or 90 degrees.
+// Using the smallest 30-60-90 triangle as a building block, we can define all other
+// triangles from one or more of these.
+
+//                              *A
+//                             /|\
+//                            / | \
+//                           /  |  \
+//                         B+_  |  _+C
+//                         /   -|-   \
+//                        /   _ *D_   \
+//                       /_ -   |F  - _\
+//                     E*-------+-------*G
+//                     /|\- _   |   _ -/|\
+//                    / | \   - *H-   / | \
+//                   /  |  \ _ -|- _ /  |  \
+//                 I+_  |  _+J  |  K+_  |  _+L
+//                 /   -|-   \  |  /   -|-   \
+//                /   _ *M_   \ | /   _ *N_   \
+//               /_ -   |   - _\|/_ -   |   - _\
+//             O*-------+-------*-------+-------*S
+//             /|\- _   |P  _ -/|\-Q_   |R  _ -/|\       
+//            / | \   - *T-   / | \   - *U-   / | \ 
+//           /  |  \ _ -|- _ /  |  \ _ -|- _ /  |  \
+//         V+_  |  _+W  |   +X  |  _+Y  |  Z+_  |  _+a
+//         /   -|-   \  |  /   -|-   \  |  /   -|-   \
+//        /   _ *b_   \ | /   _ *c_   \ | /   _ *d_   \
+//       /_ -   |   - _\|/_ -   |   - _\|/_ -   |   - _\
+//      *-------+-------*-------+-------*-------+-------*
+//      e       f       g       h       i       j       k
+//                           Size 3
+
+
+// Enumerate Size 1 triangles: 3 + 6 + 6 + 1
+//   30-30-120 x2 : ADE, ADG, DEG
+//   30-60-90  x1 : ABD, ACD, BDE, CDG, DEF, DFG
+//   30-60-90  x3 : ABG, ACE, AEF, AFG, BEG, CEG
+//   60-60-60  x6 : AEG
+
+// Enumerate Size 2 triangles: 12 + 6 + 3 + 24 + 24 + 12 + 6 + 6 + 6 + 4 + 1
+//   30-30-120 x2 : ADE, ADG, DEG, EGH, EHQ, EMO, EMQ, GHQ, GNQ, GNS, MOQ, BQS
+//   30-30-120 x6 : AEQ, AGQ, EGO, EGS, EQS, GOQ
+//   30-30-120 x8 : AHO, AHS, HOS
+//   30-60-90  x1 : ABD, ACD, BDE, CDG, DEF, DFG, EFH, EHJ, EIM, EJM, FGH, GHK, GKN, GLN, HJQ, HKQ, IMO, JMQ, KNQ, LNS, MOP, MPQ, NQR, NRS
+//   30-60-90  x3 : ABG, ACE, AEF, AFG, BEG, CEG, EFQ, EGJ, EGK, EIQ, EJO, EKQ, EOP, EPQ, FGQ, GJQ, GKS, GLQ, GQR, GRS, IOQ, JOQ, KQS, LQS
+//   30-60-90  x4 : AEH, AGH, DEQ, DGQ, EGM, EGN, EHO, ENQ, GHO, GMQ, HOQ, HQS
+//   30-60-90  x9 : AIQ, ALQ, BGO, CES, EPS, GOR
+//   30-60-90 x12 : AES, AGO, AOQ, AQS, EOS, GOS
+//   60-60-60  x2 : DEH, DGH, EHM, GHN, HMQ, HNQ
+//   60-60-60  x6 : AEG, EGQ, EOQ, GQS
+//   60-60-60 x24 : AOS
+
+
+
+// Let us define the size 1 triangle AEG as having edges of length 2.
+// Then we have these distances for the various triangle edges:
+//   AE = AG = EG = 2.
+//   AB = AC = BE = CG = EF = FG = 1
+//   AF = BG = CF = sqrt(3)
+//   AD = DE = DG = 2/sqrt(3) = 2/3*sqrt(3)
+//   BD = CD = FD = 1/3*sqrt(3)
+
+// As we can see, there are 3 families of triangles that can be enumerated: 30-30-120, 30-60-90, and 60-60-60.
+// We'll start with the 30-60-90, since they are the smallest building blocks.  These come in 2 forms, those
+// where the hypotenuse is rational and those where it has a sqrt(3) in it.
 
 #include <iostream>
 
--- ProjectEuler0164/ProjectEuler0164.cpp
+++ ProjectEuler0164/ProjectEuler0164.cpp
@@ -2,8 +2,129 @@
 // three consecutive digits of have a sum greater than 9?
 
 #include <iostream>
+#include <vector>
+
+// Let's start by enumerating the 3 digit sequences that have a sum < 10:
+//      000, 001, 002, 003, 004, 005, 006, 007, 008, 009
+//      010, 011, 012, 013, 014, 015, 016, 017, 018
+//      020, 021, 022, 023, 024, 025, 026, 027
+//      030, 031, 032, 033, 034, 035, 036
+//      040, 041, 042, 043, 044, 045
+//      050, 051, 052, 053, 054
+//      060, 061, 062, 063
+//      070, 071, 072
+//      080, 081
+//      090
+//      100, 101, 102, 103, 104, 105, 106, 107, 108
+//      110, 111, 112, 113, 114, 115, 116, 117
+//      120, 121, 122, 123, 124, 125, 126
+//      130, 131, 132, 133, 134, 135
+//      140, 141, 142, 143, 144
+//      150, 151, 152, 153
+//      160, 161, 162
+//      170, 171
+//      180
+//      200, 201, 202, 203, 204, 205, 206, 207
+//      210, 211, 212, 213, 214, 215, 216
+//      220, 221, 222, 223, 224, 225
+//      230, 231, 232, 233, 234
+//      240, 241, 242, 243
+//      250, 251, 252
+//      260, 261
+//      270
+//      300, 301, 302, 303, 304, 305, 306
+//      310, 311, 312, 313, 314, 315
+//      320, 321, 322, 323, 324
+//      330, 331, 332, 333
+//      340, 341, 342
+//      350, 351
+//      360
+//      400, 401, 402, 403, 404, 405
+//      410, 411, 412, 413, 414
+//      420, 421, 422, 423
+//      430, 431, 432
+//      440, 441
+//      450
+//      500, 501, 502, 503, 504
+//      510, 511, 512, 513
+//      520, 521, 522
+//      530, 531
+//      540
+//      600, 601, 602, 603
+//      610, 611, 612
+//      620, 621
+//      630
+//      700, 701, 702
+//      710, 711
+//      720
+//      800, 801
+//      810
+//      900
+// The sequences were organzied like this to show that the number of sequences that
+// start with the same digit is a triangle number T(10-n) = n*(n+1)/2.
+// So for 3 digit sequences, we have 55 + 45 + 36 + 28 + 21 + 15 + 10 + 6 + 3 + 1 = 220.
+
+// How to construct longer sequences?  We can look at a 4 digit number ABCD as being
+// comprised of two 3 digit sequences - ABC and BCD.  Both of these must be in the set of
+// 220 valid 3 digit numbers, but the values of A & D are completely decoupled.
+
+//       0  1  2  3  4  5  6  7  8  9
+//    0 10  9  8  7  6  5  4  3  2  1
+//    1  9  8  7  6  5  4  3  2  1  0
+//    2  8  7  6  5  4  3  2  1  0  0
+//    3  7  6  5  4  3  2  1  0  0  0
+//    4  6  5  4  3  2  1  0  0  0  0
+//    5  5  4  3  2  1  0  0  0  0  0
+//    6  4  3  2  1  0  0  0  0  0  0
+//    7  3  2  1  0  0  0  0  0  0  0
+//    8  2  1  0  0  0  0  0  0  0  0
+//    9  1  0  0  0  0  0  0  0  0  0
+
+
+uint64_t brute_force(int16_t num_digits) {
+    uint64_t ret{ 0 };
+
+    int64_t max_val{ 1 };
+    for (int16_t n = 0; n < num_digits; ++n)
+        max_val *= 10;
+
+    std::cout << max_val << std::endl;
+
+    for (int64_t n = 100; n < max_val; ++n) {
+        int64_t sum{ 0 };
+        int64_t tmp{ n };
+        int64_t d_0{ 0 };
+        int64_t d_1{ 0 };
+        int64_t d_2{ 0 };
+        for (int16_t d = 0; d < num_digits; ++d) {
+            d_0 = tmp % 10;
+
+            sum += d_0 - d_2;
+            if (sum > 9)
+                break;
+
+            d_2 = d_1;
+            d_1 = d_0;
+            tmp /= 10;
+        }
+
+        if (sum < 10) {
+            ++ret;
+            std::cout << n << "  ";
+        }
+    }
+
+    std::cout << std::endl;
+
+    return ret;
+}
+
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    {
+        std::cout << brute_force(3) << std::endl;
+    }
 }
